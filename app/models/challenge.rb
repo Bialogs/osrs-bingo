596edefbce704225fd34b0454adf70ab65449a9e
Kyle Fagan <bialogs@gmail.com>
2023-05-05 00:03:27 -0700

    Add s3 dependency and attach images that we need so far

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Challenge < ApplicationRecord
+  has_one_attached :image
+
   validates :points, :title, presence: true
 
   validate :points, numericality: { greater_than_or_equal_to: 1 }
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
+  has_one_attached :logo
+  has_one_attached :banner
+
   validates :name, presence: true
 
   with_options dependent: :nullify do
